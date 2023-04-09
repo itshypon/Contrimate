4bacf67bd0a0bfe38e21e79dc13008866ccab168
abhinavaa01 <agarwalabhinav91@gmail.com>
2023-04-09 11:31:02 +0530

    minor changes

--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -3,50 +3,57 @@ import axios from "axios"
 
 export const fetchDataIn = (url) => {
     console.log("started");
-    const status = new Promise((resolve, reject)=> {const fetchDate=async ()=>{
-        try {
-            const res=  axios.get(url)
-            console.log(res);
-            resolve(res.data.items);
-        } catch (error) {
-            reject(error);
+    const status = new Promise((resolve, reject) => {
+        const fetchDate = async () => {
+            try {
+                axios.get(url)
+                    .then(response => {
+                        console.log(response.data);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                // console.log(res);
+                // resolve(res.data.items);
+            } catch (error) {
+                reject(error);
+            }
         }
-    }
     })
     return status;
 }
 
-const useFetch= (url)=>{
-    const [data,setData]=useState([]);
-    const [loading,setLoading]=useState(false);
-    const [error,setError]=useState(false);
-    useEffect(()=>{
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
 
-        const fetchDate=async ()=>{
+        const fetchDate = async () => {
             setLoading(true);
             try {
-                const res= await axios.get(url)
+                const res = await axios.get(url)
                 setData(res.data.items);
             } catch (error) {
                 setError(error);
             }
             setLoading(false);
         }
-    
-        fetchDate();
-    },[url])
 
-    const reFetchDate=async ()=>{
+        fetchDate();
+    }, [url])
+
+    const reFetchDate = async () => {
         setLoading(true);
         try {
-            const res= await axios.get(url)
+            const res = await axios.get(url)
             setData(res.data);
         } catch (error) {
             setError(error);
         }
         setLoading(false);
     }
-    return {data,loading,error,reFetchDate};
+    return { data, loading, error, reFetchDate };
 }
 
 export default useFetch;
